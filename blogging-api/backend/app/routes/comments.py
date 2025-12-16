7d871dfbe3d507e8b7854eee53e8e8111652790c
Baygeldi <baygeldi0718@gmail.com>
2025-12-17 02:00:04 +0800

    UpdateAndDeleteCommentMethodAdded

--- blogging-api/backend/app/routes/comments.py
+++ blogging-api/backend/app/routes/comments.py
@@ -39,3 +39,34 @@ def list_comments_for_post(post_id: int, db: Session=Depends(get_db)):
         comment.author_username = comment.author.username
 
     return comments
+
+@router.put("/{comment_id}", response_model=CommentOut)
+def update_comment(comment_id: int, comment_in: CommentCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    #chech whether comment exists
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+    # ensure the current user is the author of the comment
+    if comment.author_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to edit this comment")
+    
+    comment.content = comment_in.content
+    db.commit()
+    db.refresh(comment)
+
+    return comment 
+
+@router.delete("/{comment_id}", status_code=status.HTTP_404_NOT_FOUND)
+def delete_comment(comment_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    #check whether the comment exists
+    comment = db.query(Comment).filter(Comment.id == comment_id).first()
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
+    #ensure current user is the author of this comment
+    if comment.author_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not authorized to delete this comment")
+    
+    db.delete(comment)
+    db.commit()
+
+    return None
